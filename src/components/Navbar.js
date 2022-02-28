3c62a1e6c97cae7ab4886a932e95832f6d38d1ef
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-02-28 11:13:29 +0000

    Add Router setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,42 @@
-const Navbar = () => <nav />;
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  {
+    id: 1,
+    path: '/',
+    text: 'Rockets',
+  },
+  {
+    id: 2,
+    path: '/missions',
+    text: 'Missions',
+  },
+  {
+    id: 3,
+    path: '/profile',
+    text: 'Profile',
+  },
+];
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      {links.map(({ id, path, text }) => (
+        <li key={id}>
+          <NavLink
+            to={path}
+            className="nav-link"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? 'underline' : 'none',
+              color: '#0079f9',
+            })}
+          >
+            {text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Navbar;

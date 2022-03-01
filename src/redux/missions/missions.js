96738ca9a788b417a601fc0080e30cc6519afca0
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-03-01 08:15:50 +0000

    Merged and pulled changes and changed the fetch to only once when clicked at the right component

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,66 @@
+const FETCH_MISSIONS_REQUEST = 'FETCH_MISSIONS_REQUEST';
+const FETCH_MISSIONS_SUCCESSFUL = 'FETCH_MISSIONS_SUCCESSFUL';
+const FETCH_MISSIONS_FAILURE = 'FETCH_MISSIONS_FAILURE';
+
+const fetchMissionsRequest = () => ({
+  type: FETCH_MISSIONS_REQUEST,
+});
+
+const fetchMissionsSuccess = (missions) => ({
+  type: FETCH_MISSIONS_SUCCESSFUL,
+  payload: missions,
+});
+
+const fetchMissionsFailure = (error) => ({
+  type: FETCH_MISSIONS_FAILURE,
+  payload: error,
+});
+
+const initialMissionState = {
+  loading: false,
+  missions: [],
+  error: '',
+};
+
+export const missionReducer = (state = initialMissionState, action) => {
+  switch (action.type) {
+    case FETCH_MISSIONS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_MISSIONS_SUCCESSFUL:
+      return {
+        ...state,
+        loading: false,
+        missions: action.payload,
+        error: '',
+      };
+
+    case FETCH_MISSIONS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        missions: [],
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const fetchMissions = () => async (dispatch) => {
+  dispatch(fetchMissionsRequest());
+  const request = await fetch('https://api.spacexdata.com/v3/missions');
+  try {
+    const missions = await request.json();
+    dispatch(fetchMissionsSuccess(missions));
+  } catch (error) {
+    const errorMsg = error.message;
+    dispatch(fetchMissionsFailure(errorMsg));
+  }
+};
+
+export default fetchMissions;

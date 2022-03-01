159548d597cfe872884226a4dd6a8844290f88a8
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-01 18:35:50 +0100

    added filtered reserved rockets to profile

--- src/components/Profile/MyRockets.js
+++ src/components/Profile/MyRockets.js
@@ -1,17 +1,38 @@
+import PropTypes from 'prop-types';
+import { BsFillTrashFill } from 'react-icons/bs';
 import styles from './Profile.module.css';
 
-const MyRockets = () => (
-  <>
-    {' '}
+const MyRockets = ({ data }) => {
+  const removeRocketHandler = (id) => {
+    console.log(id);
+  };
+  return (
     <ul className={styles['my-journeys']}>
       <li>
         {' '}
         <h1>My Rockets</h1>
       </li>
-      <li>Falcon 9</li>
-      <li>Falcon Heavy</li>
-      <li>Starship</li>
+      {data.length === 0 ? <li>No rocket reservations yet</li> : data.map(({ id, rocketName }) => (
+        <li key={id}>
+          <div className={styles.setItems}>
+            <span>{rocketName}</span>
+            <BsFillTrashFill onClick={() => removeRocketHandler(id)} />
+          </div>
+        </li>
+      ))}
     </ul>
-  </>
-);
+  );
+};
+
+MyRockets.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rocketName: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      reserved: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
+
 export default MyRockets;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
 import MyMissions from './MyMissions';
 import MyRockets from './MyRockets';
 import styles from './Profile.module.css';
 
-const Profile = () => (
-  <div className={styles.container}>
-    <MyMissions />
-    <MyRockets />
-  </div>
-);
+const Profile = () => {
+  const rocketState = useSelector(
+    (state) => state.rocketReducer.rockets,
+  ).filter((rocket) => rocket.reserved === true);
+
+  return (
+    <div className={styles.container}>
+      <MyMissions />
+      <MyRockets data={rocketState} />
+    </div>
+  );
+};
 
 export default Profile;
